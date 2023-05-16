a224455f2525a5baa0b1674b5acdbfa9c851d14e
Lucca Santos <lucasluccad.r@outlook.com>
2023-05-16 13:52:50 -0300

    feat(delete/route): add route delete file

--- src/routes/fileRoutes.ts
+++ src/routes/fileRoutes.ts
@@ -2,9 +2,11 @@ import { Request, Response, Router } from "express";
 
 import { UploadFileController } from "../controllers/upload-file";
 import { ReadFileController } from "../controllers/read-file";
+import { DeleteFileController } from "../controllers/delete-file";
 
 import { MongodbUploadFileRepository } from "../repositories/mongodb/upload-file";
 import { MongodbGetFileByNameRepository } from "../repositories/mongodb/get-file-by-name";
+import { MongodbDeleteFileRepository } from "../repositories/mongodb/delete-file";
 
 import { upload } from "../config/multerConfig";
 
@@ -28,6 +30,15 @@ router.get("/files/:filename", async (req: Request, res: Response) => {
     new MongodbGetFileByNameRepository()
   );
   const result = await readFileController.handle({ params: req.params });
+
+  res.status(result.statusCode).json({ message: result.body });
+});
+
+router.delete("/files/:filename", async (req: Request, res: Response) => {
+  const deleteFileController = new DeleteFileController(
+    new MongodbDeleteFileRepository()
+  );
+  const result = await deleteFileController.handle({ params: req.params });
 
   res.status(result.statusCode).json({ message: result.body });
 });
