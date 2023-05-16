bd7de12040a9c9ac1c217870d107b2172c0d6978
Lucca Santos <lucasluccad.r@outlook.com>
2023-05-16 08:40:49 -0300

    feat: add mongodb connection

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,17 @@
+import "dotenv/config";
+
 import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Conexão com banco de dados
+import db from "./database/mongodb";
+
+db.once("open", () => {
+  console.log("Conexão estabelecida com banco de dados mongodb.");
+  app.emit("logged");
+});
 
 app.use("/", (req, res) => {
   res.status(200).send("Txt Uploader API!");
@@ -11,4 +21,6 @@ app.use("*", (req, res) => {
   res.status(404).json({ error: "Rota não encontrada." });
 });
 
-app.listen(port, () => console.log(`Server rodando na porta ${port}`));
+app.on("logged", () => {
+  app.listen(port, () => console.log(`Server rodando na porta ${port}`));
+});
